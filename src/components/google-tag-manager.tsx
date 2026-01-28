cae72319ecfa3ec5a66b44680c836171a0790747
pankaj3030 <pankajmehndiratta3@gmail.com>
2026-01-28 12:07:31 +0000

    Update google-tag-manager.tsx

--- src/components/google-tag-manager.tsx
+++ src/components/google-tag-manager.tsx
@@ -9,15 +9,17 @@ interface GoogleTagManagerProps {
 export function GoogleTagManagerHead({ gtmId }: GoogleTagManagerProps) {
   return (
     <Script
-      id="gtm-script"
+      id="google-tag-manager"
       strategy="afterInteractive"
       dangerouslySetInnerHTML={{
         __html: `
+          <!-- Google Tag Manager -->
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','${gtmId}');
+          <!-- End Google Tag Manager -->
         `,
       }}
     />
